29690ef94082ae092248036afddbf474dc659c25
Cameron <cameronwood858@googlemail.com>
2026-08-28 11:14:19 +0100

    Refactor items endpoint to use ItemResponse model and update create_item function for consistency

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 import psycopg2.extras
 from api_lib.database import get_conn
-from api_lib.types import Item
+from api_lib.types import BaseItem, ItemResponse
 from fastapi import Depends, FastAPI
 from psycopg2.extensions import connection
 
@@ -15,8 +15,8 @@ def status():
     return {"status": "ok"}
 
 
-@app.get("/items/", response_model=list[Item])
-def items(conn: Annotated[connection, Depends(get_conn)]):
+@app.get("/items/", response_model=list[ItemResponse])
+def get_items(conn: Annotated[connection, Depends(get_conn)]):
     """Return all items from the database."""
     with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
         cur.execute("SELECT * FROM list")
@@ -24,3 +24,20 @@ def items(conn: Annotated[connection, Depends(get_conn)]):
         items = [dict(row) for row in cur.fetchall()]
 
     return items
+
+
+@app.post("/items/", response_model=ItemResponse)
+def create_item(
+    item: BaseItem,
+    conn: Annotated[connection, Depends(get_conn)],
+):
+    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+        cur.execute(
+            "INSERT INTO list (content) VALUES (%s) RETURNING id, content",
+            (item.content,),
+        )
+
+        created_item = dict(cur.fetchone())  # type: ignore
+        conn.commit()
+
+    return created_item
