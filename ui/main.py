6e0d5d310227665b67be598c28f1e9a1716250c9
Cameron <cameronwood858@googlemail.com>
2026-08-30 20:29:42 +0100

    Refactor task retrieval to fetch from API and update layout rendering

--- ui/main.py
+++ ui/main.py
@@ -1,20 +1,26 @@
+import requests
 from dash import Dash, html
 
 app = Dash()
 server = app.server
 
-mock_tasks = [
-    {"id": 1, "content": "Buy more milk."},
-    {"id": 2, "content": "Walk the dog."},
-    {"id": 3, "content": "Find the cat."},
-]
+
+def create_task_element(id, content):
+    return html.Li(html.Div(f"{content}"), id=f"task_{id}")
 
 
 def get_tasks():
-    return [html.Div(f"{t['id']} - {t['content']}") for t in mock_tasks]
+    return html.Ol(
+        [
+            create_task_element(task["id"], task["content"])
+            for task in sorted(
+                requests.get("http://api:8080/items/").json(), key=lambda x: x["id"]
+            )
+        ]
+    )
 
 
-app.layout = [html.H1("Todo"), *get_tasks()]
+app.layout = [html.H1("Todo"), get_tasks()]
 
 
 if __name__ == "__main__":
