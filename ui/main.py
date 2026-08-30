062506b31442539aa4920814629b21ce9230cd92
Cameron <cameronwood858@googlemail.com>
2026-08-30 20:39:59 +0100

    Refactor app layout to include task creation input and callback functionality

--- ui/main.py
+++ ui/main.py
@@ -1,5 +1,5 @@
 import requests
-from dash import Dash, html
+from dash import Dash, Input, Output, State, dcc, html
 
 app = Dash()
 server = app.server
@@ -20,7 +20,28 @@ def get_tasks():
     )
 
 
-app.layout = [html.H1("Todo"), get_tasks()]
+app.layout = [
+    html.H1("Todo"),
+    html.Div(
+        [
+            dcc.Input(id="new-task-input", type="text", placeholder="", debounce=True),
+            dcc.Button("Create", id="create-btn", n_clicks=0),
+        ],
+        style={"display": "flex"},
+    ),
+    get_tasks(),
+    html.Div(id="container-button-basic", children="Enter a value and press submit"),
+]
+
+
+@app.callback(
+    Output("container-button-basic", "children"),
+    Input("create-btn", "n_clicks"),
+    State("new-task-input", "value"),
+    prevent_initial_call=True,
+)
+def update_output(n_clicks, value):
+    requests.post("http://api:8080/items/", json={"content": value})
 
 
 if __name__ == "__main__":
